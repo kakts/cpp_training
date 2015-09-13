032658809e691d19a90676f7762bce5f381245ec
kakts <akutsukeita@gmail.com>
2015-09-13 21:00:09 +0900

    p51 accept.cpp if condition.

---
+++ chapter1/accept.cpp
@@ -0,0 +1,27 @@
+// p51
+#include <iostream>
+using namespace std;
+bool accept()
+{
+  cout << "Do you want to proceed (y or n)? \n";
+
+  char answer = 0;
+  cin >> answer;
+
+  switch (answer)
+  {
+    case 'y':
+      return true;
+    case 'n':
+      return false;
+    default:
+      cout << "I'll take that for a no. \n";
+      return false;
+  }
+}
+
+int main() {
+  bool ans = accept();
+
+  cout << "answer is " << ans << "\n";
+}
