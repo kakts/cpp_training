11b07f129d0ee3614866da74989cb5c8e754c1c1
kakts <kakts.git@gmail.com>
2017-07-04 00:24:49 +0900

    Add sample hello

--- chapter1/hello.cpp
+++ chapter1/hello.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+
+void some_function()
+{
+  double d = 2.2;
+  int i = 7;
+  d = d;
+
+  for (int i = 0; i < 10; i++) {
+    std::cout << "test" << i << "\n";
+  }
+}
+
 int main()
 {
   std::cout << "hello world\n";
+  some_function();
 }
